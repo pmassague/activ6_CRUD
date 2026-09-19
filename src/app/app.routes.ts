489d0fdfe2805bc04e6ba31262ad42b7a164b8aa
Patricia Massagué Ramos <pmassague@gmail.com>
2026-09-19 15:39:55 +0200

    Creacion de rutas y componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
+import { UserViewPage } from './pages/user-view/user-view.page';
+import { UserFormPage } from './pages/user-form/user-form.page';
+import { P404Page } from './pages/p404/p404.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomePage },
+  { path: 'user/:idUser', component: UserViewPage },
+  { path: 'newuser', component: UserFormPage },
+  { path: 'updateuser/:idUser', component: UserFormPage },
+
+  { path: '**', component: P404Page }
+];
---
+++ src/app/pages/user-form/user-form.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-user-form',
+  styleUrl: './user-form.page.css',
+  templateUrl: './user-form.page.html',
+})
+export class UserFormPage {}
